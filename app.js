6d787f62551bc32271c07957fd1c1d02120707ca
Patrick <100382449+PatrickOlintoDuarte@users.noreply.github.com>
2024-11-26 10:42:48 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 });
 
 // Iniciar o servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Usa a porta definida pelo Railway ou 3000 como fallback
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
